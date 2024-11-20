aa10f39b431d5848a3121a2627e9634861e46e9a
IQBAL HASSAN <iqbal.hassan2584@gmail.com>
2024-11-20 15:39:20 +0500

    fix confilc

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 const AuthController = () => import('#controllers/auth_controller')
 const UploadController = () => import('#controllers/upload_controller')
 
@@ -22,7 +23,7 @@ const BedController = () => import('#controllers/tenant/bed_controller')
 router.get('/', async ({ response }) => {
   response.ok({
     code: 200,
-    data: "Awal HR Management System REST API's is Started.",
+    data: "Awal Umrah Management System REST API's is Started.",
   })
 })
 
@@ -30,81 +31,84 @@ router.post('/api/v1/tenant-register', [AuthController, 'tenantRegister'])
 router.post('/api/v1/upload', [UploadController, 'imageUploader'])
 router.get('/api/v1/verify-domain/:name', [AuthController, 'verifyDomainName'])
 
-router.group(() => {
-  // booking routes
-  router
-    .group(() => {
-      router.get('/', [BookingController, 'index'])
-      router.post('/', [BookingController, 'create'])
-      router.get('/:id', [BookingController, 'show'])
-      router.put('/:id', [BookingController, 'update'])
-      router.delete('/:id', [BookingController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/booking')
-  // hotel routes
-  router
-    .group(() => {
-      router.get('/', [HotelController, 'index'])
-      router.post('/', [HotelController, 'create'])
-      router.get('/:id', [HotelController, 'show'])
-      router.put('/:id', [HotelController, 'update'])
-      router.delete('/:id', [HotelController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/hotel')
-  // room routes
-  router
-    .group(() => {
-      router.get('/', [RoomController, 'index'])
-      router.post('/', [RoomController, 'create'])
-      router.get('/:id', [RoomController, 'show'])
-      router.put('/:id', [RoomController, 'update'])
-      router.delete('/:id', [RoomController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/room')
-  // bed routes
-  router
-    .group(() => {
-      router.get('/', [BedController, 'index'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/bed')
-  // agencies routes
-  router
-    .group(() => {
-      router.get('/', [AgencyController, 'index'])
-      router.post('/', [AgencyController, 'create'])
-      router.get('/:id', [AgencyController, 'show'])
-      router.put('/:id', [AgencyController, 'update'])
-      router.delete('/:id', [AgencyController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/agencies')
-  // visa company routes
-  router
-    .group(() => {
-      router.get('/', [VisaCompanyController, 'index'])
-      router.post('/', [VisaCompanyController, 'create'])
-      router.get('/:id', [VisaCompanyController, 'show'])
-      router.put('/:id', [VisaCompanyController, 'update'])
-      router.delete('/:id', [VisaCompanyController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/visa-company')
-  // air line routes
-  router
-    .group(() => {
-      router.get('/', [AirLineController, 'index'])
-      router.post('/', [AirLineController, 'create'])
-      router.get('/:id', [AirLineController, 'show'])
-      router.put('/:id', [AirLineController, 'update'])
-      router.delete('/:id', [AirLineController, 'destroy'])
-    })
-    .use(middleware.auth({ guards: ['api'] }))
-    .prefix('/air-line')
-})
+router
+  .group(() => {
+    // booking routes
+    router
+      .group(() => {
+        router.get('/', [BookingController, 'index'])
+        router.post('/', [BookingController, 'create'])
+        router.get('/:id', [BookingController, 'show'])
+        router.put('/:id', [BookingController, 'update'])
+        router.delete('/:id', [BookingController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/booking')
+    // hotel routes
+    router
+      .group(() => {
+        router.get('/', [HotelController, 'index'])
+        router.post('/', [HotelController, 'create'])
+        router.get('/:id', [HotelController, 'show'])
+        router.put('/:id', [HotelController, 'update'])
+        router.delete('/:id', [HotelController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/hotel')
+    // room routes
+    router
+      .group(() => {
+        router.get('/', [RoomController, 'index'])
+        router.post('/', [RoomController, 'create'])
+        router.get('/:id', [RoomController, 'show'])
+        router.put('/:id', [RoomController, 'update'])
+        router.delete('/:id', [RoomController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/room')
+    // bed routes
+    router
+      .group(() => {
+        router.get('/', [BedController, 'index'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/bed')
+    // agencies routes
+    router
+      .group(() => {
+        router.get('/', [AgencyController, 'index'])
+        router.post('/', [AgencyController, 'create'])
+        router.get('/:id', [AgencyController, 'show'])
+        router.put('/:id', [AgencyController, 'update'])
+        router.delete('/:id', [AgencyController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/agencies')
+    // visa company routes
+    router
+      .group(() => {
+        router.get('/', [VisaCompanyController, 'index'])
+        router.post('/', [VisaCompanyController, 'create'])
+        router.get('/:id', [VisaCompanyController, 'show'])
+        router.put('/:id', [VisaCompanyController, 'update'])
+        router.delete('/:id', [VisaCompanyController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/visa-company')
+    // air line routes
+    router
+      .group(() => {
+        router.get('/', [AirLineController, 'index'])
+        router.post('/', [AirLineController, 'create'])
+        router.get('/:id', [AirLineController, 'show'])
+        router.put('/:id', [AirLineController, 'update'])
+        router.delete('/:id', [AirLineController, 'destroy'])
+      })
+      .use(middleware.auth({ guards: ['api'] }))
+      .prefix('/air-line')
+  })
+  .use(middleware.tenant())
+  .prefix('/api/v1')
 
 import '#start/routes/menu'
 import '#start/routes/auth'
